77967f024ba2545de3f0618d9819965df85a8d13
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-06-05 21:07:10 +0900

    done
    done

--- 06_Python_Operators/05_Operator.py
+++ 06_Python_Operators/05_Operator.py
@@ -4,7 +4,8 @@
 """
 
 Python Identity Operators
-Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+Identity operators are used to compare the objects, not if they are equal,
+ but if they are actually the same object, with the same memory location:
 
 Operator	Description	Example	Try it
 is 	Returns true if both variables are the same object	x is y	
